deb05c80a3dbf8b5bbafaff8deb2bfea235b5d68
Markus Alexander Kuppe <github.com@lemmster.de>
2014-08-20 13:34:12 +0200

    Rewrote player to better stay in sync with backend tracklist. Especially it allows to add tracks in the backend too
    Conflicts:
    	controllers/player.js
    	package.json

--- controllers/player.js
+++ controllers/player.js
@@ -14,11 +14,11 @@ function Player (app,server) {
 
 	var self = this;
 
-	this.queue = [];
-
-	this.current_track;
-
-	this.lastPlaybackEnded;
+	// We need to maintain a datastructure to store the
+	// enhanced track objects (added requestor and cover
+	// art). Otherwise, the browser won't show a cover
+	// or the name of the user who has requested the track.
+	var map = {};
 
 	events.EventEmitter.call(this);
 
@@ -30,7 +30,6 @@ function Player (app,server) {
 		'now_playing' : null,
 		'error_offline_msg' : "Music Server is offline",
 		'playbackstatus':''
-
 	};
 
 	this.online = false;
@@ -46,9 +45,7 @@ function Player (app,server) {
 	this.default_playlist = null;
 
 	this.stop = function(req, res){
-
 		// console.log('self.mopidy.tracklist',self.mopidy);
-
 		res_json.success(res,"");
 	}
 	this.pause = function(){
@@ -68,90 +65,92 @@ function Player (app,server) {
 		self.emit('player:playpause',self.status.playbackstatus);
 		res_json.success(res, {'message': self.status.playbackstatus });
 	}
-	this.add = function( req, res){
 
-
+	this.add = function(req, res){
 		if(self.online === false) return res_json.error(res, self.status.error_offline_msg);
 
 		var _track = req.body;
-		console.log('add: ', _track.name);
-		console.log('add: ', _track.uri);
-		if(self.util.check4dup(_track.uri)){
 
-			var _msg = 'Track is a duplicate';
+		// attach the name of the user who has added this track
+		_track.by = req.user || "???";
 
-			self.emit('log',_msg);
+		// Try to figure out if this track is already part of the tracklist
+		// If track is already part of tracklist, don't add it.
+		// Otherwise get the album art and add to tracklist afterwards.
+		self.mopidy.tracklist.filter({'uri': [_track.uri]}).then(function(tl_tracks) {
+			if (tl_tracks.length == 0){
+				self.util.getArt(_track,function(err,track){
+					if(err) return res_json.error(res,err);
 
-			res_json.error(res,_msg);
-
-		}else{
-
-
-			_track.by = req.user || "fart";
-
-			self.util.getArt(_track,function(err,track){
-
-				if(err) return res_json.error(res,err);
-
-				if(self.default_playlist != null){
-					self.default_playlist= null;
-					self.mopidy.playback.stop();
-					self.mopidy.tracklist.clear();
-				};
-
-				if(self.status.now_playing){
-
-					self.queue.push(_track);
-
-				}else{
-					console.log('add#lookupAndPlay');
-					self.lookupAndPlay(_track);
-
-				}
-				self.emit('player:track:added');
-				res_json.success(res,_track);
-
-			});
-
-
-
-
-		}
-
+					self.mopidy.library.lookup(_track.uri).then(function(t){
+						// Lookup track and add to tracklist
+						self.mopidy.tracklist.add(t);
+						self.emit('player:track:added');
+						map[_track.uri] = _track;
+						res_json.success(res,_track);
+					});
+				});
+			}else{
+				var _msg = 'Track is a duplicate';
+				self.emit('log',_msg);
+				res_json.error(res,_msg);
+			}
+		});
 	}
 
 
 	this.playlist = function(req, res){
 
-
-		var _playlist = self.util.buildPlaylist();
-
-		if(_playlist[0] === "empty"){
-			res_json.error(res, "No tracks added yet");
-		}else{
-			res_json.success(res, {'tracks':_playlist});
-		}
-
-
+		self.mopidy.tracklist.getTlTracks().then(function(tl_tracks){
+			if(tl_tracks.length == 0){
+				res_json.error(res, "No tracks added yet");
+			}else{
+				// Remove all tracks from tl_tracks that have already been played. Thus,
+				// get the currently playing track and slice tl_tracks accordingly.
+				self.mopidy.playback.getCurrentTlTrack().then(function(current_tl_track) {
+					self.mopidy.tracklist.index(current_tl_track).then(function(idx){
+						self.mopidy.tracklist.getLength().then(function(length){
+							self.mopidy.tracklist.slice(idx, length).then(function(tl_tracks){
+								// extract track objs from tl_track wrappers
+								var tracks = [];
+								for (var i = 0; i < tl_tracks.length; ++i) {
+									var cacheHit = map[tl_tracks[i].track.uri];
+									if (typeof cacheHit == 'undefined'){
+										// cache miss (the track has been added to the backend
+										// by another frontend)
+										cacheHit = tl_tracks[i].track;
+										// it does not have a user object associated
+										cacheHit.by = req.user;
+										cacheHit.by.name = "???";
+										cacheHit.by.id = "???";
+										cacheHit.by.username = "???";
+										// nor has it cover artwork
+										//TODO add album from looked up at spotify.com
+										cacheHit.album.art = 'images/apollo.png';
+										// Finally add to cache
+										map[cacheHit.uri] = cacheHit;
+									}
+									tracks[i] = cacheHit;
+								}
+								res_json.success(res, {'tracks':tracks});
+							});
+						});
+					});
+				});
+			}
+		});
 	}
 
 
 	this.search = function(req, res){
-
-
 		var term = req.params.query;
 		if (term.indexOf('spotify:') != -1){
-
 			self.mopidy.library.lookup(term).then(function(tracks) {
-
 				var tracks = self.util.removeAlreadyAdded(tracks);
 
-			    return res_json.success(res, {'tracks':tracks,'term':term});
-
+				return res_json.success(res, {'tracks':tracks,'term':term});
 			});
-
 		}else{
-
 			self.mopidy.library.search({any:[term]}).then(function(data){
 				// merge all result arrays of all backends
 				// (they might have duplicates though which we tolerate)
@@ -164,33 +163,10 @@ function Player (app,server) {
 					}
 				}
 				var tracks = self.util.removeAlreadyAdded(results.tracks);
-
 				return res_json.success(res, {'tracks':tracks,'term':term});
-
 			});
 
 		}
-
-	}
-
-
-	this.lookupAndPlay = function(p_track){
-
-		if (typeof p_track === 'undefined' || p_track === null) return;
-
-		this.current_track = p_track;
-
-		self.mopidy.library.lookup(p_track.uri).then(function(track) {
-
-			self.mopidy.tracklist.clear();
-
-			self.mopidy.tracklist.add(track);
-
-			console.log("lookupAndPlay: ", p_track.name);
-
-			self.play();
-
-		});
 	}
 
 	this.bomb = function(req, res){
@@ -231,144 +207,115 @@ function Player (app,server) {
 			}
 
 		}else{
-
 			current_track.bombers = {};
 			current_track.bombers[req.user.id] = "vote";
 			current_track.bomb_count = 1;
 		}
-
 		self.emit('player:bomb:update', current_track.bomb_count);
-
 		res_json.success(res, {track:current_track});
-
-
 	}
 
 
 	this.loadDefaultPlayList = function(){
-
-
 		var uri = self.default_playlist_uri;
-
-		if(uri === '') return;
-
+		if(uri === ''){
+			console.log('No default playlist uri given, thus cannot fallback to tracks on empty tracklist');
+			return;
+		}
 		self.mopidy.library.lookup(uri).then(function(tracks) {
-
-			//get an random array of 34 tracks.
-			var subsetTracks = self.util.createSubset(tracks,34);
-			self.default_playlist = subsetTracks;
-			self.mopidy.tracklist.clear();
-			self.mopidy.tracklist.add(subsetTracks);
-			self.mopidy.tracklist.shuffle();
-			self.play();
-			self.emit('playback:defaultTracks', subsetTracks);
-			console.log('playback:defaultTracks', subsetTracks);
+			self.util.shuffle(tracks);
+			self.default_playlist = tracks;
+			self.emit('playback:defaultTracks', self.default_playlist);
+			//console.log('playback:defaultTracks', self.default_playlist);
 		});
-
-		self.emit('playback:notracks');
-	    console.log('playback:notracks');
-
 	}
 
 
 	this._playbackStarted = function(track){
-
-		if(self.default_playlist != null) return;
-
-		self.status.now_playing = track;
-
 		self.emit('playback:started', track);
-
 		console.log('playback:started', track);
 	}
 
-	this._playbackEnded = function(lastTrack){
-		console.log('_playbackEnded: ', lastTrack);
+	this._playbackEnded = function(lastTlTrack){
+		console.log('_playbackEnded: ', lastTlTrack);
+
+		self.mopidy.playback.getCurrentTlTrack().then(function(tl_track) {
+			if (tl_track == null){
+				console.log('Tracklist has reached its end, add one more track from the default playlist');
+				// remove first track from default playlist and have it played
+				var track = self.default_playlist.shift();
+				self.mopidy.library.lookup(track.uri).then(function(track) {
+					self.mopidy.tracklist.add(track).then(function(addedTracks){
+						self.mopidy.playback.play(addedTracks[0]);
+						self.emit('playback:queue', [_track]);
+					});
+				});
+			}
+		});
+/*		self.mopidy.tracklist.getLength().then(function(length){
+			// The end of the tracklist has been reached, if
+			// the length of the tracklist (minus one due to array counting)
+			// is equal to the tracklist track id
+			console.log('tracklist length: ', length);
+			if((length - 1) == parseInt(lastTlTrack.tl_track.tlid)){
+				console.log('Tracklist has reached its end, add one more track from the default playlist');
+				// remove first track from default playlist and have it played
+				var track = self.default_playlist.shift();
+				self.mopidy.library.lookup(track.uri).then(function(track) {
+					self.mopidy.tracklist.add(track);
+					self.play();
+				});
+			}
+		});
+*/	}
 
 		// This check "filters" duplicate playback ended events
 		// which seem to be emitted by the javascript mopidy client.
 		// A duplicate causes Apollo to skip two or more tracks in
 		// quick succession potentially emptying the playlist.
-		if(lastTrack.tl_track.track.uri === self.lastPlaybackEnded){
+/*		if(lastTrack.tl_track.track.uri === self.lastPlaybackEnded){
 			console.log('Skipping what appears to be a duplicate event');
 			return;
 		}
 		self.lastPlaybackEnded = lastTrack.tl_track.track.uri;
 
-		if(self.default_playlist != null) {
-
-			return;
-		}
-
-
 		if(self.bomb_switch) {
 			return;
 		}
-
-		if(self.queue[0]) {
-
-			var to_be_played = self.queue.shift();
-			console.log('playbackEnded#lookupAndPlay');
-			self.lookupAndPlay(to_be_played);
-
-			self.emit('playback:queue', self.util.buildPlaylist() );
-
-			console.log('playback:queue');
-
-		} else {
-			self.status.now_playing = null;
-
-			if(self.current_track.uri == lastTrack.tl_track.track.uri){
-				self.current_track = null;
-				self.loadDefaultPlayList();
-			}
-			self.emit('playback:queue', self.util.buildPlaylist() );
-
-		}
-	}
-
+*/
 	this._online = function (){
-
 		console.info('[Player.js]: Online');
-		//console.log(self.mopidy);
-		//console.log(self.mopidy.tracklist);
-		//console.log(self.mopidy.playback);
 		self.online = true;
-		self.mopidy.tracklist.clear();
-		self.lookupAndPlay(self.current_track);
 		self.loadDefaultPlayList();
 	}
 
 	this._offline = function(){
-
 		console.info('[Player.js]: Offline');
 		this.online = false;
 	}
 
 	this._disconnect = function() {
-
 		// Close the WebSocket without reconnecting. Letting the object be garbage
 		// collected will have the same effect, so this isn't strictly necessary.
 		this.mopidy.close();
-
 		// Unregister all event listeners. If you don't do this, you may have
 		// lingering references to the object causing the garbage collector to not
 		// clean up after it.
 		this.mopidy.off();
-
 		// Delete your reference to the object, so it can be garbage collected.
 		this.mopidy = null;
-
 	}
 
-	// this.mopidy.on('state:online', this.online.bind(this));
+	//this.mopidy.on(console.log.bind(console)); // prints ALL events to console
 	this.mopidy.on("state:online", this._online.bind(this));
 	this.mopidy.on("state:offline", this._offline.bind(this));
 	this.mopidy.on('event:trackPlaybackStarted', this._playbackStarted.bind(this));
 	this.mopidy.on('event:trackPlaybackEnded', this._playbackEnded.bind(this));
+	//this.mopidy.on('event:playbackStateChanged', ...); // Called if state changes between stopped and playing (which happens after each track)
 
 	this.util = {
 		getArt : function(p_track, p_callback){
+			console.log('Getting cover for: ',p_track);
 			// Only send cover art request to spotify if track uri points to spotify
 			if (p_track.uri.substr(0, 'spotify:'.length) === 'spotify:') {
 				request({
@@ -378,7 +325,6 @@ function Player (app,server) {
 			          'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0'
 				}
 			}, function(error, response, body) {
-
 					var response = JSON.parse(body);
 					var album_art_640 = function(str){
 						str = str.split('cover');
@@ -386,65 +332,21 @@ function Player (app,server) {
 					}(response.thumbnail_url);
 
 					p_track.album.art = album_art_640;
-
 					p_callback.apply(self,[error,p_track]);
-
-
 			});
 			}else{
 				// return dummy image as track
 				p_track.album.art = 'images/apollo.png';
 				p_callback.apply(self,[null, p_track]);
 			}
-
-
 		},
-		check4dup : function(uri){
 
-			for (var x in self.queue) {
-				if(self.queue[x].uri === uri) return true
-			}
 
-			return false;
-
-		},
-		buildPlaylist : function(){
-			if(self.queue.length > 0){
-				var _tmp_array = self.queue.slice(0);
-				_tmp_array.splice(0,0,self.current_track);
-				return _tmp_array;
-			}else{
-				return [ self.current_track || "empty" ];
-			}
-
-		},
 		removeAlreadyAdded : function(tracks){
-			if(self.queue.length > 0){
-				var keepers = [];
-				for (var i = 0; i < tracks.length; i++) {
-
-					for (var x = 0; x < self.queue.length; x++) {
-
-						if(tracks[i].uri == self.queue[x].uri){
-							tracks.splice(i,1);
-
-						}
-					};
-
-				}
-				for (var y = 0; y < tracks.length; y++) {
-
-					if(self.current_track.uri == tracks[y].uri){
-						tracks.splice(y,1);
-					}
-				};
-
-				return tracks;
-			}else{
-				return tracks;
-			}
-
+			//TODO check current tracklist
+			return tracks;
 		},
+
 		createSubset: function(arr, amount){
 			var i = arr.length;
 			while (--i) {
@@ -455,6 +357,22 @@ function Player (app,server) {
 			}
 			var subset = arr.slice(0,amount);
 			return subset;
+		},
+
+		shuffle: function(array) {
+			var counter = array.length, temp, index;
+			// While there are elements in the array
+			while (counter > 0) {
+				// Pick a random index
+				index = Math.floor(Math.random() * counter);
+				// Decrease counter by 1
+				counter--;
+				// And swap the last element with it
+				temp = array[counter];
+				array[counter] = array[index];
+				array[index] = temp;
+			}
+			return array;
 		}
 	};
 }
