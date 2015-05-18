d8f8892a53d752909ecb66d25794a21c7c3c1a15
Aaron McGuire <aaron.mcguire@evb.com>
2015-05-17 20:20:03 -0700

    fixed some bootstap bugs, formatted index page with new from style. added capitolization of the username in /player view. Fixed issue with perspective change.

--- public/js/app/views/profile-view.js
+++ public/js/app/views/profile-view.js
@@ -1,32 +1,32 @@
 define([
-  'jquery',
-  'underscore',
-  'backbone',
-  'handlebars',
-  'bootstrap',
-  'notification'
-], function($, _, Backbone,Handlebars,bootstrap,Notification) {
-  'use strict';
- 
-  var ProfileView = Backbone.View.extend({
-		tagName: 'section',
-		className: 'profile row',
-		events: {
-			'click .addtrack:not(.added)': 'addtrack',
-			'click .bomb' : 'bomb_track'
-		},
-		initialize: function(){
-			this.model.on('sync', this.render,this);
-		},
-		render: function(){
-			var source = $('#profile-template').html();
-			var template = Handlebars.compile(source);
-			var html = template(this.model.toJSON());
-			this.$el.html(html);
-			
-			return this;
-		}
-	});
+    'jquery',
+    'underscore',
+    'backbone',
+    'handlebars',
+    'bootstrap',
+    'notification'
+], function($, _, Backbone, Handlebars, Notification) {
+    'use strict';
 
-  return ProfileView;
+    var ProfileView = Backbone.View.extend({
+        tagName: 'section',
+        className: 'profile row',
+        events: {
+            'click .addtrack:not(.added)': 'addtrack',
+            'click .bomb': 'bomb_track'
+        },
+        initialize: function() {
+            this.model.on('sync', this.render, this);
+        },
+        render: function() {
+            var source = $('#profile-template').html();
+            var template = Handlebars.compile(source);
+            var html = template(this.model.toJSON());
+            this.$el.html(html);
+
+            return this;
+        }
+    });
+
+    return ProfileView;
 });
