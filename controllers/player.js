b62c78f09229cefadf49fe385ebf450ee52536cc
Aaron McGuire <aaron.mcguire@evb.com>
2015-05-17 22:23:42 -0700

    okay, what am I doing

--- controllers/player.js
+++ controllers/player.js
@@ -72,7 +72,7 @@ function Player (app,server) {
 		var _track = req.body;
 
 		// attach the name of the user who has added this track
-		_track.by = req.user || "Apollo Player";
+		_track.by = req.user || "???";
 
 		// Try to figure out if this track is already part of the tracklist
 		// If track is already part of tracklist, don't add it.
